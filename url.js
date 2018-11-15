9185209fa66b220e8218a6790e5f799d2d0f5ced
German <german.sp91@gmail.com>
2018-11-15 18:32:22 -0300

    Agregados JQuery y Bootstrap al proyecto. Configurado para que tome los datos de un host remoto.

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-var URL_OGC = '/cgi-bin/qgis_mapserv.fcgi?map=/home/user/Desktop/TP Integrador/tp-integrador.qgz'
+var URL_OGC = 'http://osgeolive/cgi-bin/qgis_mapserv.fcgi?map=/home/user/Desktop/TP Integrador/tp-integrador.qgz'
