0eebec82ee70e083c246d24b466d3620d2ac4087
German <german.sp91@gmail.com>
2018-11-18 15:32:20 -0300

    Implementada IIFE con async function y uso de await para separar mejor el código. Reemplazado each (JQuery) por for-of

--- app.js
+++ app.js
@@ -9,48 +9,51 @@ var layers_op = [];
 // Instancia de Open Layers
 var map;
 
-fetch(url + '&SERVICE=WMS&REQUEST=GetCapabilities')
-.then(response => response.text())
-.then(function(data) {
-    let layers = new ol.format.WMSCapabilities().read(data).Capability.Layer.Layer;
+// async IIFE que construye la instancia del mapa y demás hierbas
+(async function() {
+    map = await fetch(url + '&SERVICE=WMS&REQUEST=GetCapabilities')
+    .then(response => response.text())
+    .then(data => {
+        let layers = new ol.format.WMSCapabilities().read(data).Capability.Layer.Layer;
 
-    $.each(layers, function(index, value) {
-        layers_op.push(
-            new ol.layer.Image({
-                title: value.Title,
-                //capa desactivada por defecto
-                visible: false,
-                source: new ol.source.ImageWMS({
-                    url: url,
-                    params: {
-                        LAYERS: value.Name
-                    }
+        for (let layer of layers) {
+            layers_op.push(
+                new ol.layer.Image({
+                    title: layer.Title,
+                    //capa desactivada por defecto
+                    visible: false,
+                    source: new ol.source.ImageWMS({
+                        url: url,
+                        params: {
+                            LAYERS: layer.Name
+                        }
+                    })
                 })
-            })
-        );
-    });
+            );
+        }
 
-    // definicion del mapa y su capa
-    map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                title: "Natural Earth Base Map",
-                source: new ol.source.TileWMS({
-                    url: 'http://demo.boundlessgeo.com/geoserver/wms',
-                    params: {'LAYERS': 'ne:ne'}
+        // definicion del mapa y su capa
+        return new ol.Map({
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({
+                    title: "Natural Earth Base Map",
+                    source: new ol.source.TileWMS({
+                        url: 'http://demo.boundlessgeo.com/geoserver/wms',
+                        params: {'LAYERS': 'ne:ne'}
+                    })
                 })
+            ].concat(layers_op),
+            view: new ol.View({
+                projection: 'EPSG:4326',
+                center: [-59, -27.5],
+                zoom: 6
             })
-        ].concat(layers_op),
-        view: new ol.View({
-            projection: 'EPSG:4326',
-            center: [-59, -27.5],
-            zoom: 6
-        })
+        });
     });
 
     let zoom_ctrl = $('.ol-zoom');
 
     zoom_ctrl.css('bottom', zoom_ctrl.css('top')).css('right', zoom_ctrl.css('left'));
     zoom_ctrl.css('top', 'unset').css('left', 'unset');
-});
+})();
